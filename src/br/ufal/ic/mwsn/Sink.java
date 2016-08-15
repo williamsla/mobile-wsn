6006946f95c29815e651a1d7342ff1eb13c3d0ca
unknown <andreducap@gmail.com>
2016-08-15 02:30:37 -0300

    GetProcessedData

--- src/br/ufal/ic/mwsn/Sink.java
+++ src/br/ufal/ic/mwsn/Sink.java
@@ -2,11 +2,9 @@ package br.ufal.ic.mwsn;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
+
 
 
 public class Sink extends Node {
@@ -19,7 +17,7 @@ public class Sink extends Node {
 			System.out.println(i + ":" + dataArray[i].toString());
 		}
 	}
-
+	
 	public String[] formatData() {
 
 		String dataArray[] = this.getData().split(";");
@@ -27,50 +25,12 @@ public class Sink extends Node {
 		// System.out.println("Sink data: " + dataArray.toString());
 		return dataArray;
 	}
-	private String getMD5Hash(String str) throws NoSuchAlgorithmException{
-		
-		MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(str.getBytes());
-
-        byte byteData[] = md.digest();
-
-        //convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < byteData.length; i++) {
-         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        //System.out.println("Digest(in hex format):: " + sb.toString());
-
-        //convert the byte to hex format method 2
-        StringBuffer hexString = new StringBuffer();
-    	for (int i=0;i<byteData.length;i++) {
-    		String hex=Integer.toHexString(0xff & byteData[i]);
-   	     	if(hex.length()==1) hexString.append('0');
-   	     	hexString.append(hex);
-    	}
-    	return hexString.toString();
-    		
-		
+	public String[] getProcessedData(){
+		String dataArray[] = formatData();
+		dataArray = eliminateDuplicate(dataArray);
+		return dataArray;
 	}
-	
-	public Map<String, Statistics> getStaticsperData(String dataArray[]) throws NoSuchAlgorithmException{
-		Map<String, Statistics> statiscsMap = new HashMap<>();
-		
-		for (String string : dataArray) {
-			String key = getMD5Hash(string);
-			if(statiscsMap.containsKey(key)){
-				statiscsMap.get(key).incrementSaltos();
-			}
-			else{
-				Statistics statistics = new Statistics();
-				statistics.incrementSaltos();
-				statiscsMap.put(key, statistics);
-			}
-			
-		}				
-		return statiscsMap;
-	}
+
 
 	public String[] eliminateDuplicate(String dataArray[]) {
 		for (int i = 0; i < dataArray.length; i++) {
--- src/br/ufal/ic/mwsn/Statistics.java
+++ src/br/ufal/ic/mwsn/Statistics.java
@@ -1,5 +1,10 @@
 package br.ufal.ic.mwsn;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statistics {
 	private long saltos;
 
@@ -9,5 +14,49 @@ public class Statistics {
 
 	public void incrementSaltos() {
 		this.saltos += saltos;
+	}
+	private String getMD5Hash(String str) throws NoSuchAlgorithmException{
+		
+		MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+
+        byte byteData[] = md.digest();
+
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //System.out.println("Digest(in hex format):: " + sb.toString());
+
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	return hexString.toString();
+    		
+		
+	}
+	
+	public Map<String, Statistics> getStaticsperData(String dataArray[]) throws NoSuchAlgorithmException{
+		Map<String, Statistics> statiscsMap = new HashMap<>();
+		
+		for (String string : dataArray) {
+			String key = getMD5Hash(string);
+			if(statiscsMap.containsKey(key)){
+				statiscsMap.get(key).incrementSaltos();
+			}
+			else{
+				Statistics statistics = new Statistics();
+				statistics.incrementSaltos();
+				statiscsMap.put(key, statistics);
+			}
+			
+		}				
+		return statiscsMap;
 	}
 }
