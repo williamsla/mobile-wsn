3e74a049f91c0ee7458b2c90378a753d4d875b58
unknown <andreducap@gmail.com>
2016-08-15 02:06:40 -0300

    estatisca por dado

---
+++ src/br/ufal/ic/mwsn/Statistics.java
@@ -0,0 +1,13 @@
+package br.ufal.ic.mwsn;
+
+public class Statistics {
+	private long saltos;
+
+	public long getSaltos() {
+		return saltos;
+	}
+
+	public void incrementSaltos() {
+		this.saltos += saltos;
+	}
+}
